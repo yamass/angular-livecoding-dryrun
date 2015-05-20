313660353d5f4cb64deb994312d9158ab0bd2b62
Yann Massard <yamass@gmail.com>
2015-05-20 13:57:10 +0200

    gulp watch

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
 
 gulp.task('bower', function () {
     return bower()
@@ -14,6 +20,21 @@ gulp.task('minify-js', ['bower'], function() {
                 path.basename += ".min";
             }))
             .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', function () {
+    return gulp.src(['less/*.less'])
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(postcss([autoprefixer({browsers: ['> 2%']})]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist/css'))
+            .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(['less/*.less'], ['less']);
 });
 
 gulp.task('default', ['bower', 'minify-js']);
